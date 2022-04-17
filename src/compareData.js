8e6a112d492b54277c873ce91c973b73b38a07a8
Dmitriy Zakharov <dzaharov794@gmail.com>
2022-04-17 19:04:52 +0300

    Add compareData function

---
+++ src/compareData.js
@@ -0,0 +1,28 @@
+import _ from 'lodash';
+
+const compareData = (data1, data2) => {
+  const keys = Object.keys({ ...data1, ...data2 });
+  const sortedKeys = _.sortBy(keys);
+  return sortedKeys.map((key) => {
+    const value1 = data1[key];
+    const value2 = data2[key];
+
+    if (!_.has(data1, key)) {
+      return { type: 'added', name: key, value: value2 };
+    }
+
+    if (!_.has(data2, key)) {
+      return { type: 'remove', name: key, value: value1 };
+    }
+
+    if (!_.isEqual(value1, value2)) {
+      return {
+        type: 'changed', name: key, value1, value2,
+      };
+    }
+
+    return { type: 'unchanged', name: key, value: value1 };
+  });
+};
+
+export default compareData;
--- src/index.js
+++ src/index.js
@@ -1,6 +1,11 @@
 import process from 'process';
 import path from 'path';
 import { readFileSync } from 'fs';
+import compareData from './compareData.js';
+
+const render = (data) => {
+  console.log(data);
+};
 
 const getFilePath = (filename) => path.resolve(process.cwd(), filename);
 const readFile = (filename) => readFileSync(getFilePath(filename), 'utf-8');
@@ -8,6 +13,11 @@ const readFile = (filename) => readFileSync(getFilePath(filename), 'utf-8');
 const genDiff = (fileName1, fileName2) => {
   const file1 = readFile(fileName1);
   const file2 = readFile(fileName2);
+  const data1 = JSON.parse(file1);
+  const data2 = JSON.parse(file2);
+  const diffData = compareData(data1, data2);
+
+  return render(diffData);
 };
 
 export default genDiff;
