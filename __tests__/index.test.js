e493f23bffe9509afde748b24685339bb28f1fe8
Dmitriy Zakharov <dzaharov794@gmail.com>
2022-04-26 07:53:46 +0300

    Replace tests flat files with nested files

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -10,7 +10,7 @@ const __dirname = dirname(__filename);
 const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
 const readFile = (filename) => readFileSync(getFixturePath(filename), 'utf-8');
 
-test('Compare two flat files', () => {
-  expect(genDiff(getFixturePath('file1.json'), getFixturePath('file2.json'))).toEqual(readFile('plainFiles'));
-  expect(genDiff(getFixturePath('file1.yml'), getFixturePath('file2.yml'))).toEqual(readFile('plainFiles'));
+test('Compare two files', () => {
+  expect(genDiff(getFixturePath('file1.json'), getFixturePath('file2.json'))).toEqual(readFile('nestedFiles'));
+  expect(genDiff(getFixturePath('file1.yml'), getFixturePath('file2.yml'))).toEqual(readFile('nestedFiles'));
 });
