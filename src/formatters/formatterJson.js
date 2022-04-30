be91ec6900d4d65740b84f88f98d5347ba2d5e16
Dmitriy Zakharov <dzaharov794@gmail.com>
2022-04-30 22:06:00 +0300

    Add formatter JSON

---
+++ src/formatters/formatterJson.js
@@ -0,0 +1,3 @@
+const formatterJson = (data) => JSON.stringify(data, null, 2);
+
+export default formatterJson;
--- src/formatters/index.js
+++ src/formatters/index.js
@@ -1,5 +1,6 @@
 import styllish from './stylish.js';
 import plain from './plain.js';
+import json from './formatterJson.js';
 
 const formatter = (data, format) => {
   switch (format) {
@@ -7,6 +8,8 @@ const formatter = (data, format) => {
       return styllish(data);
     case 'plain':
       return plain(data);
+    case 'json':
+      return json(data);
     default:
       throw new Error(`${format} not supported`);
   }
